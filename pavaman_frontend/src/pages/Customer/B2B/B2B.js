23327c025229cff029843f79531a20e14443faa2
kapilitskillhub <kapilitskillhub@gmail.com>
2025-05-21 16:42:49 +0530

    varsha

--- pavaman_frontend/src/pages/Customer/B2B/B2B.js
+++ pavaman_frontend/src/pages/Customer/B2B/B2B.js
@@ -15,13 +15,13 @@ const B2B = () => {
 
         {/* Phone Line */}
         <div className="b2b-contact-line">
-          <FaPhoneAlt className="contact-icon phone-icon" />
+          <FaPhoneAlt className="contact-icon phone-icon" />&nbsp;
           <span className="b2b-contact-text">+1-800-123-4567</span>
         </div>
 
         {/* Email Line */}
         <div className="b2b-contact-line">
-          <FaEnvelope className="contact-icon email-icon" />
+          <FaEnvelope className="contact-icon email-icon" />&nbsp;&nbsp;
           <span className="b2b-contact-text">
             Email us at <a href="mailto:b2b@pavaman.com">b2b@pavaman.com</a>
           </span>
